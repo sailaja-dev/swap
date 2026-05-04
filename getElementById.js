6bf9dd1904ccfacf0f611bdf6c619fe3ad590f89
sailaja-dev <psailaja90@gmail.com>
2026-05-04 18:07:10 +0530

    Fourth commit

--- getElementById.js
+++ getElementById.js
@@ -1,9 +1,11 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+
+mainHeading.style.color = 'orange';
 const header = document.getElementById('header');
-header.textContent = 'Fruit World';
-header.style.color = 'orange';
 header.style.background = 'green';
-header.style.borderBottom = 'solid orange';
+header.style.borderBottom = 'orange';
 const basketHeading = document.getElementById('basket-heading');
 basketHeading.style.color = 'green';
 const thanks = document.getElementById('thanks');
-thanks.innerHTML = '<p> Please visit us again </p>';
+thanks.innerHTML = '<p>Please visit us again</p>';
